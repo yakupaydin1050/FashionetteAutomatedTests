50e6dfc6d4396e8ec1e938c4a55ed1b25425729d
yakupaydin <yakupaydin1050@gmail.com>
2022-02-04 01:58:54 +0100

    CukesRunner and FailedTestRunner files were updated.

--- src/test/java/de/fashionette/runners/CukesRunner.java
+++ src/test/java/de/fashionette/runners/CukesRunner.java
@@ -1,6 +1,23 @@
 package de.fashionette.runners;
 
-public class CukesRunner {
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith (Cucumber.class)
+@CucumberOptions(
+
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html.reports",
+                "rerun:target/rerun.txt"},
+        features = "src/test/resources/features",
+        glue = "de/fashionette/step_definitions",
+        dryRun = true,
+        tags= "@scenario01"
+)
+
+
+public class CukesRunner {
 
 }
--- src/test/java/de/fashionette/runners/FailedTestRunner.java
+++ src/test/java/de/fashionette/runners/FailedTestRunner.java
@@ -1,4 +1,19 @@
 package de.fashionette.runners;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions (
+
+        plugin = {"html:target/failed-html-report"},
+        features = "@target/rerun.txt",
+        glue = "de/fashionette/step_definitions"
+
+)
+
+
 public class FailedTestRunner {
 }
